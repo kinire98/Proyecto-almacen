e7de3d0951f00a1df1d112a3d2c81f965ff389a4
Juan Gutierrez Gutierrez <gurruny1@gmail.com>
2023-11-20 10:00:32 +0100

    Se han añadido los getters y setters para los almacenes.

--- Almacen.java
+++ Almacen.java
@@ -2,8 +2,30 @@ public class Almacen {
     private String direccion;
     private int superficie;
     private Repartidor repartidor;
-
-
+ 
+    public String getDireccion(){
+        return direccion;
+    }
+    
+    public int getSuperficie(){
+        return superficie;
+    }
+    
+    public Repartidor getRepartidor(){
+        return repartidor;
+    }
+    
+    public void setDireccion(String direccion){
+        this.direccion = direccion;
+    }
+    
+    public void setSuperficie(int superficie){
+        this.superficie = superficie;
+    }
+    
+    public void setRepartidor(Repartidor repartidor){
+        this.repartidor = repartidor;
+    }
 
 
 
