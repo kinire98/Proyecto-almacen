77ca22a77780cfeb10a992ff80292d2d3dee4b17
Juan Gutierrez Gutierrez <gurruny1@gmail.com>
2023-11-20 10:00:54 +0100

    Se han añadido los getters y setters para los repartidores.

--- Repartidor.java
+++ Repartidor.java
@@ -3,7 +3,29 @@ public class Repartidor {
     private int antiguedad;
     private Furgoneta furgoneta;
 
-
+    public String getNombreCompleto(){
+        return nombreCompleto;
+    }
+    
+    public int getAntiguedad(){
+        return antiguedad;
+    }
+    
+    public Furgoneta getFurgoneta(){
+        return furgoneta;
+    }
+    
+    public void setNombreCompleto(String nombreCompleto){
+        this.nombreCompleto = nombreCompleto;
+    }
+    
+    public void setAntiguedad(int antiguedad){
+        this.antiguedad = antiguedad;
+    }
+    
+    public void setFurgoneta(Furgoneta furgoneta){
+        this.furgoneta = furgoneta;
+    }
 
 
 
