2afac8c7f31a494e6def581fed3749b6a617af70
kinire98 <iknitgarri@outlook.com>
2023-11-20 09:39:44 +0100

    Añadidos los métodos getDetalles e imprimirDetalles en la Clase Almacen

--- Almacen.java
+++ Almacen.java
@@ -2,4 +2,15 @@ public class Almacen {
     private String direccion;
     private int superficie;
     private Repartidor repartidor;
+
+
+
+
+
+    public String getDetalles() {
+        return "\nDireccion: " + direccion + "\nSuperficie: " + superficie + "\nRepartidor: " + repartidor.getDetalles();
+    }
+    public void imprimirDetalles() {
+        System.out.println(getDetalles());
+    }
 }
