e87e322238b19e72a9b1bbe1ea5afaf6437211da
kinire98 <iknitgarri@outlook.com>
2023-11-20 09:42:25 +0100

    Añadidos los métodos getDetalles e imprimirDetales en la clase Repartidor

--- Repartidor.java
+++ Repartidor.java
@@ -2,4 +2,17 @@ public class Repartidor {
     private String nombreCompleto;
     private int antiguedad;
     private Furgoneta furgoneta;
+
+
+
+
+
+    public String getDetalles () {
+        return "\nNombre completo: " + nombreCompleto + "\n" +
+                "Antigüedad: " + antiguedad + "\n" +
+                "Furgoneta" + furgoneta.getDetalles() + "\n";
+    }
+    public void imprimirDetalles() {
+        System.out.println(getDetalles());
+    }
 }
